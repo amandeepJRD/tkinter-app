8c685f421d71d344a46680203d21a5d2857b6850
amandeepJRD <156605935+amandeepJRD@users.noreply.github.com>
2024-01-23 00:03:58 +0530

    Add files via upload
    Adding main file

---
+++ main.py
@@ -0,0 +1,30 @@
+import tkinter, PyPDF2
+from tkinter import filedialog
+
+
+def open_file():
+    filename = filedialog.askopenfilename(title="Open PDF file",
+                                          initialdir='/Users/sharmaaman030/Downloads',
+                                          filetypes=[('PDF files', '*.pdf')])
+    print(filename)
+
+    filename_label.configure(text=filename)
+    outputfile_text.delete("1.0", tkinter.END)
+    reader = PyPDF2.PdfReader(filename)
+    for i in range(len(reader.pages)):
+        current_text = reader.pages[i].extract_text()
+        outputfile_text.insert(tkinter.END, current_text)
+
+
+root = tkinter.Tk()
+root.title("PDF Text Extractor")
+
+filename_label = tkinter.Label(root, text="No File Selected")
+outputfile_text = tkinter.Text(root)
+openfile_button = tkinter.Button(root, text="Open PDF File", command=open_filem)
+
+filename_label.pack()
+outputfile_text.pack()
+openfile_button.pack()
+
+root.mainloop()
